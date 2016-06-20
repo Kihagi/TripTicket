b95ca181b19413f2b93c74a6c4d840cb86292495
mfuon2 <mfolee@gmail.com>
2016-06-20 15:56:26 +0300

    a missing trip.js file

---
+++ src/main/webapp/js/app/trip.js
@@ -0,0 +1,35 @@
+var trip = {
+		list: function(){
+	    	var ajax = new XMLHttpRequest();
+	    	
+	    	ajax.onreadystatechange = function(){
+	    		
+	    		if(ajax.readyState == 4){
+	    			if(ajax.status == 200){
+	    				document.getElementById('ajax-content').innerHTML = ajax.responseText;
+	    			}
+	    		}
+	    	}
+	    	
+	    	ajax.open("GET", "./trips", true);
+	    	ajax.send();
+		},
+		add: function(){
+	    	var ajax = new XMLHttpRequest();
+	    	
+	    	ajax.onreadystatechange = function(){
+	    		
+	    		if(ajax.readyState == 4){
+	    			if(ajax.status == 200){
+	    				document.getElementById('ajax-content').innerHTML = ajax.responseText;
+	    			}
+	    		}
+	    	}
+	    	
+	    	ajax.open("GET", "./trip/addTrip.jsp", true);
+	    	ajax.send();
+		},
+		
+}
+
+

d6de6ca5c81f384cce98ea2cddd2da83f855889c
duncodi <duncodi@gmail.com>
2016-06-23 17:29:30 +0300

    Ticketing implementation first draft

--- src/main/webapp/js/app/ticket.js
+++ src/main/webapp/js/app/ticket.js
@@ -1,5 +1,5 @@
 var ticket = {
-		list: function(){
+		listCompanies: function(){
 	    	var ajax = new XMLHttpRequest();
 	    	
 	    	ajax.onreadystatechange = function(){
@@ -12,6 +12,22 @@ var ticket = {
 	    	}
 	    	
 	    	ajax.open("GET", "./ticket", true);
+	    	ajax.send();
+		},
+		
+		successbooking: function(){
+	    	var ajax = new XMLHttpRequest();
+	    	
+	    	ajax.onreadystatechange = function(){
+	    		
+	    		if(ajax.readyState == 4){
+	    			if(ajax.status == 200){
+	    				document.getElementById('ajax-content').innerHTML = ajax.responseText;
+	    			}
+	    		}
+	    	}
+	    	
+	    	ajax.open("GET", "./ticket/action/*", true);
 	    	ajax.send();
 		},
 		
@@ -38,27 +54,41 @@ var ticket = {
 					}
 				}
 			}
-			ajax.open("GET", "./book.jsp?trip="+tripId, true);
+			ajax.open("GET", "./ticket/book.jsp?trip="+tripId, true);
 			ajax.send();
 		},
 		complete: function(){
-			var ajax = new XMLHttpRequest();
-			
-			ajax.onreadystatechange = function(){
-				if(ajax.readyState ==4 ){
-					if(ajax.status == 200){
-						document.getElementById('ajax-content').innerHTML = ajax.responseText;
-					}
-				}
-			}
-			ajax.open("GET", "./completebooking", true);
-			ajax.send();
+			var me = this;
+	    	var ajax = new XMLHttpRequest();
+	    	
+	    	var idNumber = document.getElementById('idNumber').value;
+	    	var name = document.getElementById('name').value;
+	    	var phone = document.getElementById('phone').value;
+	    	var seat = document.getElementById('seat').value;
+	    	var tripId = document.getElementById('tripId').value;
+	    	
+	    	var params = 'idNumber=' + encodeURIComponent(idNumber) + '&name=' + encodeURIComponent(name) + '&phone=' + encodeURIComponent(phone) + '&seat=' + encodeURIComponent(seat) + '&tripId=' + encodeURIComponent(tripId);
+	    	
+	    	console.log(params);
+	    	
+	    	ajax.onreadystatechange = function(){
+	    		
+	    		if(ajax.readyState == 4){
+	    			if(ajax.status == 200){
+	    				me.successbooking();
+	    			}
+	    		}
+	    	}
+	    	
+	    	ajax.open("POST", "./ticket/action/*", true);
+	    	ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	    	ajax.send(params);
 		}
 		
 }
 
 function ticketCompanies(){
-	ticket.list();
+	ticket.listCompanies();
 }
 
 function ticketselectVehicle(){

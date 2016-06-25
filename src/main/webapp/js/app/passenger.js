c62c14003d621a7322340eb27e48f299751dad2f
Brian Ndirangu <ndikraxx@gmail.com>
2016-06-25 09:45:24 +0300

    Passenger improved AJAX

--- src/main/webapp/js/app/passenger.js
+++ src/main/webapp/js/app/passenger.js
@@ -1,48 +1,85 @@
-var passenger = {
-		ajaxCommon: function(){
-			var ajax=new XMLHttpRequest();
-			ajax.onreadystatechange=function(){
-				if (ajax.readyState==4 && ajax.status==200){
-					
-				}
-			}
-			
-		},
-	list : function() {
-
-		ajaxCommon.open("GET", "./passengers/action", true);
-		ajaxCommon.send();
-	},
-
-	add : function() {
-		
-		ajaxCommon.open("GET", "./passengers/addpassengers.jsp", true);
-		ajaxCommon.send();
-	},
+function ajaxAll(method, url, boolean, content, encoder, params) {
 	
+	this.method = method;
+	this.url = url;
+	this.boolean = boolean;
+	this.params=params;
+	this.content = 'Content-Type';
+	this.encoder = 'application/x-www-form-urlencoded';
 
-	submit : function() {
-		var refresh = this;
-		var ajax = new XMLHttpRequest();
+	var ajax = new XMLHttpRequest();
 
-		var lastName = document.getElementById('lname').value;
-		var firstName = document.getElementById('fname').value;
-	 	var params = 'lastname=' + encodeURIComponent(lastName) 
-		+ 'firstname=' + encodeURIComponent(firstName);
-	
-	console.log(params);
+	ajax.onreadystatechange = function() {
 
-		ajax.onreadystatechange = function() {
-
-			if (ajax.readyState == 4) {
-				if (ajax.status == 200) {
-					refresh.list();
-				}
+		if (ajax.readyState == 4) {
+			if (ajax.status == 200) {
+				document.getElementById('ajax-content').innerHTML = ajax.responseText;
 			}
 		}
+	}
+	ajax.open(method, url, boolean);
+		ajax.setRequestHeader(content, encoder);
+	
+	ajax.send(params);
+};
 
-		ajax.open("POST", "./passengers/action/add", true);
-		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		ajax.send(params);
+
+
+var passenger = {
+	components : [ {
+		id : 'fname',
+		name : 'firstname',
+		placeholder : 'Enter your first name here',
+		type : 'text',
+		required : 'required'
+	}, {
+		id : 'lname',
+		name : 'lastname',
+		placeholder : 'Enter your last name here',
+		type : 'text',
+		required : 'required'
+
+	} ],
+
+	list : function() {
+		return url = new ajaxAll("GET", "./passengers/action/", true);
+	
+	},
+	add : function() {
+		var context = this;
+		var form = '<form>';
+
+		context.components.forEach(function(el) {
+			form += '<fieldset class="form-group">' + ' <input type="'
+					+ el.type + '" class="form-control" id="' + el.id
+					+ '"  name="' + el.name + '" placeholder="'
+					+ el.placeholder + '" required>' + '</fieldset>';
+
+		})
+		form += '</form>'
+				+ '<a class="btn btn-success"  onclick="passenger.save()">Submit</a>';
+		document.getElementById('ajax-content').innerHTML = form;
+
+	},
+
+	// submitForm: true,
+	save : function() {
+		var me = this;
+		// me.submitForm = true;
+
+		var params = '';
+		me.components.forEach(function(el) {
+			var val = document.getElementById(el.id).value;
+			if (val)
+				// me.submitForm = false
+				params += el.name + '=' + encodeURIComponent(val) + '&';
+		});
+		content = 'Content-Type';
+		encoder = 'application/x-www-form-urlencoded';
+		return url = new ajaxAll("POST", "./passengers/action/add", true, content, encoder, params);
+
+		
+		
+
 	}
 }
--- src/main/webapp/passengers/addpassengers.jsp
+++ src/main/webapp/passengers/addpassengers.jsp
@@ -1,11 +1,3 @@
-<form action="./passengers/action/" method="post">
-  <fieldset class="form-group">
-    <input type="text" class="form-control" id="fname"  name="firstname" placeholder="Enter your first name" required>
-  </fieldset>
-  <fieldset class="form-group">
-    <input type="text" class="form-control" id="lname"  name="lastname" placeholder="Enter your last name" required>
-  </fieldset>
-<button class="btn btn-success" onclick="passenger.submit()">Save</button>
-</form>
+
 
 
